047f23f51c4b8644b21e499e672e571500a4f8df
aklacza <andrewklacza@gmail.com>
2016-01-26 15:30:08 -0500

    commit 2

--- src/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-//created by Andrew Klacza
-
-public class HelloWorld {
-
-	public static void main(String[] args) {
-		System.out.println("Hello world!");
-	}
-
-}
---
+++ src/Main.java
@@ -0,0 +1,12 @@
+//created by Andrew Klacza
+
+public class Main {
+
+	public static void main(String[] args) {
+		for(int x = 0; x< 2; x++){
+			System.out.println("Hello world!");
+		}
+		
+	}
+
+}
